e2cce2ec5088f3b509ba59aae22c9ae4561cf096
JoseDionizio <antoniodioniziosystem@gmail.com>
2022-01-20 16:12:56 -0400

    ajsute rota

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var hostname = 'localhost';
 
 express()
-  .use(express.static(path.join(__dirname, 'dist')))
-  .use('/htmltodocx',express.static(path.join(__dirname, 'dist')))
-  .use('/deletefile',express.static(path.join(__dirname, 'dist')))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('dist/index'))  
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use('/', express.static(path.join(__dirname, '/dist')))
+  .use('/htmltodocx',express.static(path.join(__dirname, '/dist')))
+  .use('/deletefile',express.static(path.join(__dirname, '/dist')))
+  //.set('view engine', 'ejs')
+  //.get('/', (req, res) => res.render('dist/index'))  
+  .listen(PORT, hostname, () => console.log(`Listening on ${ PORT }`))
