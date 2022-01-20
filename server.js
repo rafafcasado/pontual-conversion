308f6cc7015ad55d5421dd5e09657ef560e35e62
JoseDionizio <antoniodioniziosystem@gmail.com>
2022-01-20 16:06:07 -0400

    ajsute rota

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'dist')))
-  .use('/about',express.static(path.join(__dirname, 'dist')))
-  .use('/contact',express.static(path.join(__dirname, 'dist')))
+  .use('/htmltodocx',express.static(path.join(__dirname, 'dist')))
+  .use('/deletefile',express.static(path.join(__dirname, 'dist')))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('dist/index'))  
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
